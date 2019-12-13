2d1f8a19fdaa561c6457ac26c4df7dba5aa655b3
Student <example@example.com>
2019-12-13 09:59:30 -0800

    create logic to check if both inputs are words

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,14 +4,14 @@ class Anagram
     @input2 =input2
   end
 
-  # def upper_case
-  #   @input1.upcase!
-  #   return @input1
-  # end
-
   def anagrams
     if (@input1.upcase.chars.sort == @input2.upcase.chars.sort)
       return "these words are anagrams"
+    end
+  end
+  def word_checker
+    if ((@input1.count("aeiouAEIOU") == 0) | (@input2.count("aeiouAEIOU") == 0))
+      return "one of these is not a word"
     end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -11,4 +11,8 @@ describe('Anagram#anagrams') do
     anagram = Anagram.new("HEY", "hye")
     expect(anagram.anagrams()).to(eq("these words are anagrams"))
   end
+  it("should return these words are anagrams if true") do
+    anagram = Anagram.new("hey", "hey")
+    expect(anagram.word_checker()).to(eq("one of these is not a word"))
+  end
 end
